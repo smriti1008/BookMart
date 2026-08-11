15aea992eec66c9898ff904a10aad3d2726add0a
Smriti Singh <smritisingh9118@gmail.com>
2026-08-11 13:15:07 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-zinc-800 text-zinc-300 text-center py-3 sm:py-4">
       <p className="text-sm sm:text-base">
-        © {new Date().getFullYear()}, Made with <span role="img" aria-label="heart">❤️</span> by <span className="font-semibold">Manoj Kumar</span>
+        © {new Date().getFullYear()}, Made with <span role="img" aria-label="heart">❤️</span> by <span className="font-semibold">Smriti Singh</span>
       </p>
     </footer>
   );
